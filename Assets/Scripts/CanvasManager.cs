036a20b56aa2019b826d26d3cbca83bc55a5a707
Iordache Paul-Tiberiu <iordache.tiberiu19@yahoo.com>
2025-01-06 21:33:10 +0100

    change popups with canvas

---
+++ Assets/Scripts/CanvasManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class CanvasManager : MonoBehaviour
+{
+    public GameObject canvas; // Référence au Canvas
+    public PlayerMovement playerMovement; // Référence au script PlayerMovement
+
+    void Start()
+    {
+        // Afficher le Canvas et désactiver le mouvement du joueur au début
+        canvas.SetActive(true);
+        playerMovement.canMove = false;
+    }
+
+    void Update()
+    {
+        // Vérifier si la touche Entrée est pressée
+        if (canvas.activeSelf && Input.GetKeyDown(KeyCode.Return))
+        {
+            CloseCanvas();
+        }
+    }
+
+    public void CloseCanvas()
+    {
+        // Désactiver le Canvas et activer le mouvement du joueur
+        canvas.SetActive(false);
+        playerMovement.canMove = true;
+    }
+}
--- Assets/Scripts/PaintDetection.cs
+++ Assets/Scripts/PaintDetection.cs
@@ -1,19 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class PaintDetection : MonoBehaviour
 {
     public LayerMask paint;
     public Transform cameraPos;
     public float pointerDistance;
     bool frontDetection;
-
     // Update is called once per frame
     void Update()
     {
         frontDetection = Physics.Raycast(cameraPos.position, cameraPos.forward, pointerDistance, paint);
         // Affiche le raycast dans la scène
         Debug.DrawRay(cameraPos.position, cameraPos.forward * pointerDistance, frontDetection ? Color.green : Color.red);
+
+        Debug.Log(frontDetection);
     }
 }
