f736386b68487b94777452d96d9b237bea09d6fd
Kvn00000 <94387120+Kvn00000@users.noreply.github.com>
2025-01-31 16:18:12 +0100

    bloquer rotation

--- Assets/Scripts/MouseControl.cs
+++ Assets/Scripts/MouseControl.cs
@@ -8,8 +8,13 @@ public class MouseControl : MonoBehaviour
     public float sensY;
     public Transform orientation;
     
+    public bool playerRotation;
     private float xRotation;
     private float yRotation;
+
+    private void Awake() {
+        playerRotation = true;
+    }
 
     private void Start(){
         // Cursor.lockState = CursorLockMode.Locked;
@@ -18,14 +23,16 @@ public class MouseControl : MonoBehaviour
 
 
     void Update(){
-        float mouseX = Input.GetAxisRaw("Mouse X") * sensX * Time.deltaTime;
-        float mouseY = Input.GetAxisRaw("Mouse Y") * sensY * Time.deltaTime;
+        if(playerRotation){
+            float mouseX = Input.GetAxisRaw("Mouse X") * sensX * Time.deltaTime;
+            float mouseY = Input.GetAxisRaw("Mouse Y") * sensY * Time.deltaTime;
 
-        yRotation += mouseX;
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+            yRotation += mouseX;
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0f);
-        orientation.rotation = Quaternion.Euler(0f, yRotation, 0f);
+            transform.rotation = Quaternion.Euler(xRotation, yRotation, 0f);
+            orientation.rotation = Quaternion.Euler(0f, yRotation, 0f);
+        }
     }
 }
